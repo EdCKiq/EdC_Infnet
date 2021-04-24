df053ce9b4b844d145fbc70b2d79576ec0cd29f0
EdCKiq <kaiquehelper2019@gmail.com>
2021-04-24 14:20:54 -0300

    Testando projetos e funções

---
+++ DotNET/ASP_Mod/Aulas/WatchProj/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WatchProj
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo!");
+            Console.WriteLine("Testando o \'dotnet watch run\'");
+            var nome = "Kaique";
+            Console.WriteLine(nome);
+
+
+        }
+    }
+}
